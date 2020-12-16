eedd8d5d239938f4324433c0dfb0670b5807ffd8
Coby <cobyrpilpel2@gmail.com>
2020-12-15 19:02:13 -0500

    commit tree 55d25a331076164df267d7990381f9a22bdd33d9

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,9 @@
-def my_each # put argument(s) here
-  # code here
+def my_each(array)
+i = 0
+while i < array.length
+  yield array[i]
+  i+=1
 end
+return array
+end
+## ok but why would I use each on this to begin with. Get the how don't get the why. 
